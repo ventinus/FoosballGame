6b7a04360b94409ddcd44b37e8b894cd3e96b70b
ventinus <jonradgray@gmail.com>
2022-05-15 09:37:09 -0700

    update sendScore to python3

--- src/utils/deviceHandlers/sendScore.py
+++ src/utils/deviceHandlers/sendScore.py
@@ -5,10 +5,8 @@ UDP_PORT = 5005
 UDP_IP = sys.argv[1]
 MESSAGE = sys.argv[2]
 
-print "sending message:", MESSAGE
+print("sending message:", MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-
-print "UPDATE_SCORE_SUCCESS"
+sock.sendto(MESSAGE.encode('utf-8'), (UDP_IP, UDP_PORT))
